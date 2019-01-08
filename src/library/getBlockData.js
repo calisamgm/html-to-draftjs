cf477dc0eb70d38830d2bd979883a659a03ff50a
Sa Ca <calisadev@gmail.com>
2019-01-08 13:19:20 +0700

    Handle line-height in block.

--- src/library/getBlockData.js
+++ src/library/getBlockData.js
@@ -3,14 +3,15 @@ import { Map } from 'immutable';
 export default function getBlockData(
   node: Object
 ): Object {
+  const data = {};
   if (node.style.textAlign) {
-    return new Map({
-      'text-align': node.style.textAlign,
-    })
-  } else if (node.style.marginLeft) {
-    return new Map({
-      'margin-left': node.style.marginLeft,
-    })
+    data['text-align'] = node.style.textAlign;
   }
-  return undefined;
+  if (node.style.marginLeft) {
+    data['margin-left'] = node.style.marginLeft;
+  }
+  if (node.style.lineHeight) {
+    data['line-height'] = node.style.lineHeight;
+  }
+  return new Map(data);
 }
